f438dfb39f73f128a696d105c39a94b83697791c
yinxiaojie <2378246547@qq.com>
2018-09-06 23:48:32 +0800

    yxj

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ window.onload=function () {
         let mdate=now.getMinutes();
         let sdate=now.getSeconds();
         console.log(sdate);
-        h.style.transform=`rotate(${hdate*30}deg)`;
-        m.style.transform=`rotate(${mdate*6}deg)`;
-        s.style.transform=`rotate(${sdate*6}deg)`;
+        h.style.transform=`rotate(${(hdate*3600+mdate*60+sdate)/(3600*12)*360}deg)`;
+        m.style.transform=`rotate(${(mdate*6)}deg)`;
+        s.style.transform=`rotate(${sdate/60*360}deg)`;
     }
 };
